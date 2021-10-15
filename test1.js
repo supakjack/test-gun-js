778b423fbd20f7d13e97f236fc9a6322736e1a98
unknown <48548611+supakjack@users.noreply.github.com>
2021-10-15 21:05:47 +0700

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,28 @@
+const getCountry = async () => {
+  return [
+    { name: 'Thailand', abbr: 'th', remark: '', regionId: 1 },
+    { name: 'United State', abbr: 'US', remark: '', regionId: 7 },
+    { name: 'Singapore', abbr: 'sg', remark: 'This is sg', regionId: 1 }
+  ]
+}
+
+const getRegion = async () => {
+  return [
+    { id: 1, name: 'Asia' },
+    { id: 2, name: 'South America' },
+    { id: 7, name: 'North America' }
+  ]
+}
+
+const main = async () => {
+  const region = await getRegion()
+  const country = await getCountry()
+  const result = {}
+  await country.map((c) => {
+    const r = region.find((r) => r.id == c.regionId)
+    result[c.abbr] = r.name
+  })
+  console.log(result)
+}
+
+main()
